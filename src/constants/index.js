b9df2ad60b90f0a91797e64d4c8ba3903a53e1d4
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2024-08-26 14:06:46 +0000

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,10 @@
-import ecom from "../assets/projects/ecom.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import rest from "../assets/projects/rest.jpg";
-import airport from "../assets/projects/airport.jpg";
-import medy from "../assets/projects/medy.jpg";
+import shopping from "../assets/projects/shopping.png";
+import task from "../assets/projects/task.png";
+import rest from "../assets/projects/rest.png";
+import airport from "../assets/projects/airport.png";
+import medisynth from "../assets/projects/medisynth.jpg";
+import uiux from "../assets/projects/uiux.png";
+
 
 
 export const HERO_CONTENT = `I'm a front-end enthusiast with a flair for designing sleek and interactive web interfaces.My journey with cutting-edge technologies like React and Next.js has equipped me with the skills to not only design beautiful interfaces but also to ensure they are fast, scalable, and a joy to use.Driven by creativity and a relentless pursuit of excellence, I thrive on pushing the boundaries of what's possible on the web. I meticulously fine-tune every pixel and every line of code, ensuring that the end product is not just functional but also a masterpiece in its own right. My ultimate goal is to create digital experiences that leave a lasting impression, drive business growth, and delight users at every interaction.`;
@@ -12,7 +14,7 @@ export const ABOUT_TEXT = `I'm a front-end developer with a knack for crafting v
 export const PROJECTS = [
   {
     title: "E-Commerce Website",
-    image: ecom,
+    image: shopping,
     description:
       "A e-commerce website with features like product listing, shopping cart, and product quantity.",
     technologies: ["HTML", "CSS", "JS","Bootstrap"],
@@ -20,7 +22,7 @@ export const PROJECTS = [
   },
   {
     title: "Task Management App",
-    image: project2,
+    image: task,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
     technologies: ["HTML", "CSS", "React", "MongoDb"],
@@ -45,9 +47,18 @@ export const PROJECTS = [
   },
   {
     title: "AI Virtual Patient",
-    image: medy,
+    image: medisynth,
     description:
       "A pivotal feature introduces an AI-generated patient exhibiting lifelike symptoms and presenting a simulated body temperature. Students should diagnose and prescribe medications, and then they receive real-time feedback with alternative suggestions through a user-friendly interface. The application will take the access of camera and microphone to get the data from students so as to provide an immersive learning experience. ",
+    technologies: ["HTML", "CSS","JS","Typescript","Python3"],
+    link: ["https://github.com/SanskariTechies/MediSynth"]
+  
+  },
+
+  {
+    title: "Tetherfi Internship",
+    image: uiux,
+    description: "Mobile application for customer support agents.The project aimed to develop a sophisticated backend system designed to handle user authen-tication, communication through SMS and calls, and email notifications.",
     technologies: ["HTML", "CSS","JS","Typescript","Python3"],
     link: ["https://github.com/SanskariTechies/MediSynth"]
   
@@ -55,7 +66,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "Patna,Bihar ",
-  phoneNo: "+91 1234567890 ",
-  email: "a@gmail.com",
+  address: "Room No. 307 Sahyadri Boy's Hostel Adyar , Mangalore ",
+  phoneNo: "+91 8804339456 ",
+  email: "ashishgoswami1013@gmail.com",
 };
